9cae71bfd5075fbb30928ec3388bc02dce660fe2
fatima6287 <fatimazahid6287@gmail.com>
2017-10-16 09:50:07 +0500

    food

---
+++ app/src/main/java/com/example/fatima/fast_food_system/loginpage.java
@@ -0,0 +1,13 @@
+package com.example.fatima.fast_food_system;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class loginpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginpage);
+    }
+}
